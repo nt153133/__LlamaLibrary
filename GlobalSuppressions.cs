35cc03455a031b70f8448e0507189502b4eb1590
Manta <manta@bismuth.dev>
2021-11-13 15:31:53 -0500

    editorconfig: required braces; hid CS0649 'var never assigned'

--- GlobalSuppressions.cs
+++ GlobalSuppressions.cs
@@ -6,7 +6,5 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "I don't like .This", Scope = "namespaceanddescendants", Target = "~N:LlamaLibrary")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:LlamaLibrary.LlamaMarket")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:LlamaLibrary.LlamaMarket")]
 [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Never intended to be a public API. This is fine.")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1636:FileHeaderCopyrightTextMustMatch", Justification = "Private API. Not worried about legalese.")] 
