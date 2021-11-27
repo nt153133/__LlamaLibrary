cdcc16ab1c3c84ac8de1a2efe85fc70c0b1cadf8
nt153133 <52430037+nt153133@users.noreply.github.com>
2021-11-27 03:37:12 -0500

    Remove botbases

--- LlamaBotBases/LlamaUtilities/Tasks/BotTask.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LlamaBotBases.LlamaUtilities.Tasks
-{
-    public class BotTask
-    {
-        public TaskType Type { get; set; }
-
-        public string TaskInfo { get; set; }
-    }
-}
