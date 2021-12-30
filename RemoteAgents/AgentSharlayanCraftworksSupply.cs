903f8f726934eb2606f7c7e5a8bdea99a201b96e
nt153133 <52430037+nt153133@users.noreply.github.com>
2021-12-30 00:34:14 -0500

    SharlayanCraftworksSupply window and agent

---
+++ RemoteAgents/AgentSharlayanCraftworksSupply.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ff14bot;
+using ff14bot.Managers;
+using LlamaLibrary.Memory.Attributes;
+
+namespace LlamaLibrary.RemoteAgents
+{
+    public class AgentSharlayanCraftworksSupply : AgentInterface<AgentSharlayanCraftworksSupply>, IAgent
+    {
+        public static class Offsets
+        {
+            [Offset("Search 48 8D 05 ? ? ? ? 33 F6 48 89 01 48 8B D9 48 8D 05 ? ? ? ? Add 3 TraceRelative")]
+            internal static IntPtr Vtable;
+
+            [Offset("Search 48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 57 48 83 EC ? 48 8B 41 ? 48 8D 79 ? 48 8B D9 41 8B F0")]
+            internal static IntPtr HandIn;
+
+            //0x28
+            [Offset("Search FF 50 ? 84 C0 0F 84 ? ? ? ? 48 8B CF E8 ? ? ? ? 84 C0 0F 84 ? ? ? ? 48 8B 43 ? Add 2 Read8")]
+            internal static int PointerOffset;
+        }
+
+        public IntPtr RegisteredVtable => Offsets.Vtable;
+
+        protected AgentSharlayanCraftworksSupply(IntPtr pointer) : base(pointer)
+        {
+        }
+
+        public void HandIn(BagSlot slot)
+        {
+            lock (Core.Memory.Executor.AssemblyLock)
+            {
+                Core.Memory.CallInjected64<uint>(Offsets.HandIn,
+                                                 Pointer + Offsets.PointerOffset,
+                                                 slot.Slot,
+                                                 (int) slot.BagId);
+            }
+        }
+    }
+}
---
+++ RemoteWindows/SharlayanCraftworksSupply.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Buddy.Coroutines;
+using ff14bot.Managers;
+using ff14bot.RemoteAgents;
+using ff14bot.RemoteWindows;
+using LlamaLibrary.RemoteAgents;
+
+namespace LlamaLibrary.RemoteWindows
+{
+    public class SharlayanCraftworksSupply : RemoteWindow<SharlayanCraftworksSupply>
+    {
+        private const string WindowName = "SharlayanCraftworksSupply";
+
+        public SharlayanCraftworksSupply() : base(WindowName)
+        {
+        }
+
+        public static readonly Dictionary<string, int> Properties = new Dictionary<string, int>
+        {
+            {
+                "TurnInItemId",
+                8
+            },
+            {
+                "EsteemLevel",
+                6
+            },
+            {
+                "Esteem",
+                7
+            },
+        };
+
+        public int TurnInItemId => Elements[Properties["TurnInItemId"]].TrimmedData;
+
+        public int EsteemLevel => Elements[Properties["EsteemLevel"]].TrimmedData;
+
+        public int Esteem => Elements[Properties["Esteem"]].TrimmedData;
+
+        public void Deliver()
+        {
+            SendAction(1, 3, 0);
+        }
+
+        public async Task HandOverItems()
+        {
+            var slots = InventoryManager.FilledSlots.Where(i => i.TrueItemId == TurnInItemId).OrderByDescending(i => i.Collectability).Take(6 - Esteem);
+
+            foreach (var slot in slots)
+            {
+                if (slot != null)
+                {
+                    AgentSharlayanCraftworksSupply.Instance.HandIn(slot);
+                    await Coroutine.Sleep(700);
+                }
+            }
+
+            await Coroutine.Sleep(500);
+
+            Deliver();
+
+            await Coroutine.Wait(5000, () => Talk.DialogOpen);
+
+            while (Talk.DialogOpen)
+            {
+                Talk.Next();
+                await Coroutine.Sleep(1000);
+            }
+
+            await Coroutine.Wait(5000, () => QuestLogManager.InCutscene);
+
+            while (QuestLogManager.InCutscene || Talk.DialogOpen)
+            {
+                if (QuestLogManager.InCutscene && AgentCutScene.Instance.CanSkip)
+                {
+                    AgentCutScene.Instance.PromptSkip();
+                    await Coroutine.Wait(5000, () => SelectString.IsOpen);
+                    if (SelectString.IsOpen)
+                    {
+                        SelectString.ClickSlot(0);
+                    }
+                }
+
+                if (Talk.DialogOpen)
+                {
+                    Talk.Next();
+                    await Coroutine.Sleep(200);
+                }
+
+                await Coroutine.Sleep(500);
+            }
+        }
+    }
+}
