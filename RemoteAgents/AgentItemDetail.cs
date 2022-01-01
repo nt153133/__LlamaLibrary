027f05f1d7c0b0ddfe43b8a2b050fd007dd42612
nt153133 <52430037+nt153133@users.noreply.github.com>
2021-12-31 23:40:39 -0500

    AgentItemDetail for hover over

---
+++ RemoteAgents/AgentItemDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ff14bot;
+using ff14bot.Managers;
+using LlamaLibrary.Memory.Attributes;
+using LlamaLibrary.RemoteAgents;
+
+namespace NavigationTest
+{
+    public class AgentItemDetail : AgentInterface<AgentItemDetail>, IAgent
+    {
+        private static class Offsets
+        {
+            //0x18C9FC0
+            [Offset("Search 48 8D 05 ? ? ? ? 48 8B F9 48 89 01 48 8D 05 ? ? ? ? 48 89 41 ? C6 81 ? ? ? ? ? Add 3 TraceRelative")]
+            internal static IntPtr Vtable;
+
+            //0x5
+            [Offset("Search 8B 93 ? ? ? ? 48 8B CB E8 ? ? ? ? 4C 63 F0 Add 2 Read32")]
+            internal static int ItemID;
+        }
+
+        protected AgentItemDetail(IntPtr pointer) : base(pointer)
+        {
+        }
+
+        public IntPtr RegisteredVtable => Offsets.Vtable;
+
+        public uint HoverOverItemID => Core.Memory.Read<uint>(Pointer + Offsets.ItemID);
+    }
+}
