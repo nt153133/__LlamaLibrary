3fef9fe1ca5ce7828b8a8538eeeb193362bf1830
domesticwarlord86 <57776315+domesticwarlord86@users.noreply.github.com>
2022-01-12 14:35:00 -0600

    Added GrandCompanyRankUp

---
+++ RemoteWindows/GrandCompanyRankUp.cs
@@ -0,0 +1,17 @@
+﻿namespace LlamaLibrary.RemoteWindows
+{
+    public class GrandCompanyRankUp : RemoteWindow<GrandCompanyRankUp>
+    {
+        private const string WindowName = "GrandCompanyRankUp";
+
+        public GrandCompanyRankUp() : base(WindowName)
+        {
+            _name = WindowName;
+        }
+
+        public void Confirm()
+        {
+            SendAction(1, 3, 0);
+        }
+    }
+}
