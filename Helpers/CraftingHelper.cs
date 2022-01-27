66b62a84158a21401f9c074c00d56521744f6bec
nt153133 <52430037+nt153133@users.noreply.github.com>
2022-01-27 16:32:29 -0500

    Hasrecipe arrays

--- Helpers/CraftingHelper.cs
+++ Helpers/CraftingHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Media;
 using ff14bot;
 using ff14bot.Managers;
@@ -22,20 +24,64 @@ namespace LlamaLibrary.Helpers
 
             [Offset("Search 40 53 48 83 EC ? 8B D9 81 F9 ? ? ? ?")]
             internal static IntPtr HasCraftedRecipe;
+
+            [Offset("Search 4C 8D 1D ? ? ? ? 88 44 24 ? Add 3 TraceRelative")]
+            internal static IntPtr QuestPointer;
+
+            [Offset("Search 81 F9 ? ? ? ? 72 ? 32 C0 48 83 C4 ? Add 2 Read32")]
+            internal static int NumberOfRecipes;
+
+            [Offset("Search 81 F9 ? ? ? ? 73 ? 44 0F B6 84 01 ? ? ? ?  Add 2 Read32")]
+            internal static int LengthOfArray;
+
+            [Offset("Search 44 0F B6 84 01 ? ? ? ? 0F B6 C3 Add 5 Read32")]
+            internal static int OffsetRecipes;
         }
 
         public static CraftingStatus Status => Core.Memory.Read<CraftingStatus>(Offsets.DohLastAction);
 
         public static bool AnimationLocked => CraftingManager.AnimationLocked;
 
+        public static byte[] CraftedRecipeByteArray => Core.Memory.ReadBytes(Offsets.QuestPointer + Offsets.OffsetRecipes, Offsets.LengthOfArray);
+
         public static bool HasCraftedRecipe(ushort recipeId)
         {
             using (Core.Memory.TemporaryCacheState(false))
             {
-                return Core.Memory.CallInjected64<bool>(
-                                                        Offsets.HasCraftedRecipe,
+                return Core.Memory.CallInjected64<bool>(Offsets.HasCraftedRecipe,
                                                         recipeId);
             }
+        }
+
+        public static ushort[] CraftedRecipes()
+        {
+            var craftedList = GetCraftedRecipeStatusArray().Select((crafted, index) => (crafted, index)).Where(i => i.crafted).Select(i => (ushort)i.index);
+
+            return craftedList.ToArray();
+        }
+
+        public static ushort[] NotCraftedRecipes()
+        {
+            var craftedList = GetCraftedRecipeStatusArray().Select((crafted, index) => (crafted, index)).Where(i => !i.crafted).Select(i => (ushort)i.index);
+
+            return craftedList.ToArray();
+        }
+
+        public static bool[] GetCraftedRecipeStatusArray()
+        {
+            var byteArray = CraftedRecipeByteArray;
+            bool[] isCrafted = new bool[byteArray.Length * 8];
+
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                byte b = byteArray[i];
+                for (int j = 0; j < 7; j++)
+                {
+                    isCrafted[(i * 8) + j] = (128 >> j & b) != 0;
+                }
+            }
+
+            return isCrafted;
         }
 
         public static bool IsValid(CraftingStatus status)
@@ -55,7 +101,7 @@ namespace LlamaLibrary.Helpers
                 var status = Status;
                 if (IsValid(status))
                 {
-                    return (int)status.Quality;
+                    return (int) status.Quality;
                 }
                 else
                 {
@@ -71,7 +117,7 @@ namespace LlamaLibrary.Helpers
                 var status = Status;
                 if (IsValid(status))
                 {
-                    return (int)status.Step;
+                    return (int) status.Step;
                 }
                 else
                 {
@@ -87,7 +133,7 @@ namespace LlamaLibrary.Helpers
                 var status = Status;
                 if (IsValid(status))
                 {
-                    return (int)status.HQ;
+                    return (int) status.HQ;
                 }
                 else
                 {
@@ -103,7 +149,7 @@ namespace LlamaLibrary.Helpers
                 var status = Status;
                 if (IsValid(status))
                 {
-                    return (int)status.Durability;
+                    return (int) status.Durability;
                 }
                 else
                 {
@@ -119,7 +165,7 @@ namespace LlamaLibrary.Helpers
                 var status = Status;
                 if (IsValid(status))
                 {
-                    return (int)status.Progress;
+                    return (int) status.Progress;
                 }
                 else
                 {
@@ -145,9 +191,9 @@ namespace LlamaLibrary.Helpers
         }
 
         public static int ProgressRequired => CraftingManager.ProgressRequired;
-        public static int DurabilityCap => (int)Synthesis.GetProperty("DurabilityCap");
-        public static int QualityCap => (int)Synthesis.GetProperty("QualityCap");
-        public static int IconId => (int)Synthesis.GetProperty("IconId");
+        public static int DurabilityCap => (int) Synthesis.GetProperty("DurabilityCap");
+        public static int QualityCap => (int) Synthesis.GetProperty("QualityCap");
+        public static int IconId => (int) Synthesis.GetProperty("IconId");
         public static bool IsCrafting => CraftingManager.IsCrafting;
         public static bool CanCraft => CraftingManager.CanCraft;
         public static RecipeData CurrentRecipe => CraftingManager.CurrentRecipe;
