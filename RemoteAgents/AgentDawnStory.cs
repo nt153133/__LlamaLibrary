2773b5f64aed1f3df031573363b2ee2dc86984cc
nt153133 <52430037+nt153133@users.noreply.github.com>
2022-04-12 23:36:48 -0400

    DawnStory window/agent

---
+++ RemoteAgents/AgentDawnStory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ff14bot;
+using ff14bot.Managers;
+using LlamaLibrary.Memory.Attributes;
+
+namespace LlamaLibrary.RemoteAgents
+{
+    public class AgentDawnStory : AgentInterface<AgentDawnStory>, IAgent
+    {
+        public IntPtr RegisteredVtable => Offsets.Vtable;
+
+        private static class Offsets
+        {
+            [Offset("48 8D 05 ? ? ? ? C6 43 ? ? 48 89 03 48 8B C3 48 C7 43 ? ? ? ? ? Add 3 TraceRelative")]
+            internal static IntPtr Vtable;
+        }
+
+        protected AgentDawnStory(IntPtr pointer) : base(pointer)
+        {
+        }
+
+        public byte SelectedDuty => Core.Memory.Read<byte>(Pointer + 0x28);
+    }
+}
