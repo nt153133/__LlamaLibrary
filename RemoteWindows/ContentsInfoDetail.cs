1bca6762cf7bb87d2dcd38594b07966fad54df2d
nt153133 <52430037+nt153133@users.noreply.github.com>
2022-04-23 02:18:47 -0400

    GC Crafting/Gathering Turning. Don't know if CN is broke

--- RemoteWindows/ContentsInfoDetail.cs
+++ RemoteWindows/ContentsInfoDetail.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ff14bot.Enums;
 using ff14bot.Managers;
 using ff14bot.RemoteWindows;
 using LlamaLibrary.Enums;
+using LlamaLibrary.Helpers;
 
 namespace LlamaLibrary.RemoteWindows
 {
@@ -11,29 +13,29 @@ namespace LlamaLibrary.RemoteWindows
     public class ContentsInfoDetail : RemoteWindow<ContentsInfoDetail>
     {
         private const string WindowName = "ContentsInfoDetail";
-        private int eleNumCrafting = 51;
-        private int eleNumGathering = 52;
-        private int eleCraftingItem = 205;
-        private int eleCraftingJob = 213;
-        private int eleCraftingQty = 221;
-        private int eleGatheringItem = 264;
-        private int eleGatheringJob = 267;
-        private int eleGatheringQty = 270;
+        private int eleNumCrafting = 56;
+        private int eleNumGathering = 57;
+        private int eleCraftingItem = 214;
+        private int eleCraftingJob = 222;
+        private int eleCraftingQty = 230;
+        private int eleGatheringItem = 273;
+        private int eleGatheringJob = 276;
+        private int eleGatheringQty = 279;
 
         public ContentsInfoDetail() : base(WindowName)
         {
             _name = WindowName;
-            /*if (Translator.Language == Language.Chn)
+            if (Translator.Language == Language.Chn)
             {
-                eleNumCrafting = 55;
-                eleNumGathering = 56;
-                eleCraftingItem = 206;
-                eleCraftingJob = 217;
-                eleCraftingQty = 225;
-                eleGatheringItem = 268;
-                eleGatheringJob = 271;
-                eleGatheringQty = 274;
-            }*/
+                eleNumCrafting = 51;
+                eleNumGathering = 52;
+                eleCraftingItem = 205;
+                eleCraftingJob = 213;
+                eleCraftingQty = 221;
+                eleGatheringItem = 264;
+                eleGatheringJob = 267;
+                eleGatheringQty = 270;
+            }
         }
 
         public int GetNumberOfCraftingTurnins()
