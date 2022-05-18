4bf133991689162532f8cc485ee7de266272570d
domesticwarlord86 <domesticwarlord86@gmail.com>
2022-05-17 22:07:21 -0500

    Added BeginnersMansionProblem windows.

---
+++ RemoteWindows/BeginnersMansionProblem.cs
@@ -0,0 +1,30 @@
+﻿namespace LlamaLibrary.RemoteWindows
+{
+    /// <summary>
+    ///     Class for using the Hall of Noivce Window
+    /// </summary>
+    public class BeginnersMansionProblem : RemoteWindow<BeginnersMansionProblem>
+    {
+        private const string WindowName = "BeginnersMansionProblem";
+
+        public BeginnersMansionProblem() : base(WindowName)
+        {
+            _name = WindowName;
+        }
+
+        public void Begin()
+        {
+            SendAction(1, 3, 0);
+        }
+
+        public void Close()
+        {
+            SendAction(1, 3, 1);
+        }
+
+        public void DisplayeCompletedExercises()
+        {
+            SendAction(1, 3, 2);
+        }
+    }
+}
---
+++ RemoteWindows/BeginnersMansionProblemCo.cs
@@ -0,0 +1,31 @@
+﻿namespace LlamaLibrary.RemoteWindows
+{
+    /// <summary>
+    ///     Class for using the Hall of Noivce Window
+    /// </summary>
+    public class BeginnersMansionProblemCo : RemoteWindow<BeginnersMansionProblemCo>
+    {
+        private const string WindowName = "BeginnersMansionProblemCo";
+
+        public BeginnersMansionProblemCo() : base(WindowName)
+        {
+            _name = WindowName;
+        }
+
+        /// <summary>
+        ///     Sets the squadron command mission.
+        /// </summary>
+        /// <param name="index">The duty index from the list starting at 0.</param>
+        public void SelectExercise(int index)
+        {
+            SendAction(2, 3, 0, 4, (ulong)index);
+        }
+
+        public void Close()
+        {
+            SendAction(1, 3, 1);
+        }
+
+
+    }
+}
