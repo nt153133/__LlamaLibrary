45a6ea10e68753459982461c4c87f06e429a6718
domesticwarlord86 <domesticwarlord86@gmail.com>
2022-06-12 11:59:10 -0500

    Added window

---
+++ RemoteWindows/HugeCraftworksSupplyResul.cs
@@ -0,0 +1,18 @@
+﻿namespace LlamaLibrary.RemoteWindows
+{
+    //TODO Move element numbers to dictionary
+    public class HugeCraftworksSupplyResul : RemoteWindow<HugeCraftworksSupplyResul>
+    {
+        private const string WindowName = "HugeCraftworksSupplyResul";
+
+        public HugeCraftworksSupplyResul() : base(WindowName)
+        {
+            _name = WindowName;
+        }
+
+        public void Accept()
+        {
+            SendAction(1, 3, 0);
+        }
+    }
+}
