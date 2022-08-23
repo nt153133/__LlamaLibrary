1656dc9a69321eab36fcbe5539b2e4ae6f4c51ac
domesticwarlord86 <domesticwarlord86@gmail.com>
2022-08-23 06:07:16 -0500

    Added MJIHud template

---
+++ RemoteWindows/MJIHud.cs
@@ -0,0 +1,17 @@
+﻿namespace LlamaLibrary.RemoteWindows
+{
+    public class MJIHud : RemoteWindow<MJIHud>
+    {
+        private const string WindowName = "MJIHud";
+
+        public MJIHud() : base(WindowName)
+        {
+            _name = WindowName;
+        }
+
+        public override void Close()
+        {
+            SendAction(1, 3, 0);
+        }
+    }
+}
