5b3baba86ad40c9971a7e23603f2804cd67edbed
Manta <manta@bismuth.dev>
2022-10-16 19:05:47 -0400

    Add Unending Codex window

---
+++ RemoteWindows/AkatsukiNote.cs
@@ -0,0 +1,9 @@
+﻿namespace LlamaLibrary.RemoteWindows
+{
+    public class AkatsukiNote : RemoteWindow<AkatsukiNote>
+    {
+        public AkatsukiNote() : base("AkatsukiNote")
+        {
+        }
+    }
+}
