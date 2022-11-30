3a9fc3f0e8f928ad936746ef475370738cf3b967
nt153133 <52430037+nt153133@users.noreply.github.com>
2022-11-29 20:47:52 -0500

    Assembly proxy

---
+++ AssemblyProxy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using ff14bot;
+using LlamaLibrary.Memory;
+using Newtonsoft.Json.Serialization;
+
+namespace LlamaLibrary
+{
+    public static class AssemblyProxy
+    {
+        public static Assembly OnCurrentDomainOnAssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var assemblyName = new AssemblyName(args.Name);
+            switch (assemblyName.Name)
+            {
+                case "Newtonsoft":
+                case "Newtonsoft.Json":
+                    return typeof(JsonContract).Assembly;
+                case "GreyMagic":
+                    return Core.Memory.GetType().Assembly;
+                case "ff14bot":
+                    return Core.Me.GetType().Assembly;
+                case "LlamaLibrary":
+                    return typeof(OffsetManager).Assembly;
+                case "Clio.Localization":
+                    return typeof(Infralution.Localization.Wpf.CultureManager).Assembly;
+                default:
+                    return null!;
+            }
+        }
+    }
+}
