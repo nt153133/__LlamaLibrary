8ebc05d84a3ef30e2b40d96f8478b92375c12705
domesticwarlord86 <domesticwarlord86@gmail.com>
2023-03-16 19:27:51 -0500

    Added SpearFishing window

---
+++ RemoteWindows/SpearFishing.cs
@@ -0,0 +1,9 @@
+﻿namespace LlamaLibrary.RemoteWindows
+{
+    public class SpearFishing : RemoteWindow<SpearFishing>
+    {
+        public SpearFishing() : base("SpearFishing")
+        {
+        }
+    }
+}
