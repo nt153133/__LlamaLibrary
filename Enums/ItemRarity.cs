9a1c3b7c9ca77c3ef9e619db32405d91b4cd154a
Manta <manta@bismuth.dev>
2023-06-17 09:31:41 -0400

    Add ItemRarity enum by item color

---
+++ Enums/ItemRarity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LlamaLibrary.Enums;
+
+public enum ItemRarity : byte
+{
+    None = 0,
+    White = 1,
+    Green = 2,
+    Blue = 3,
+    Purple = 4,
+    Pink = 7,
+}
