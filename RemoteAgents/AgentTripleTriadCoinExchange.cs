76e631a883d4b365289de8a92c3ef37719f0f099
nt153133 <52430037+nt153133@users.noreply.github.com>
2023-07-25 14:46:17 -0400

    Start of AgentTripleTriadCoinExchange

---
+++ RemoteAgents/AgentTripleTriadCoinExchange.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ff14bot;
+using ff14bot.Managers;
+using LlamaLibrary.Memory.Attributes;
+using LlamaLibrary.Structs;
+
+namespace LlamaLibrary.RemoteAgents;
+
+public class AgentTripleTriadCoinExchange : AgentInterface<AgentTripleTriadCoinExchange>, IAgent
+{
+    public IntPtr RegisteredVtable => Offsets.VTable;
+
+    private static class Offsets
+    {
+        [Offset("Search 3B 79 ? 0F 83 ? ? ? ? 48 8B 41 ? Add 2 Read8")]
+        internal static int CardCount;
+
+        [Offset("Search 48 03 59 ? 48 8B CB 41 89 3C 06 Add 3 Read8")]
+        internal static int ListPtr;
+
+        [Offset("Search 48 8D 05 ? ? ? ? 48 8B D3 48 8D 4F ? 48 89 07 E8 ? ? ? ? 48 8B 5C 24 ? 33 C0 48 89 47 ? 48 89 47 ? 48 89 47 ? 48 89 47 ? 89 87 ? ? ? ? Add 3 TraceRelative")]
+        internal static IntPtr VTable;
+    }
+
+    public int CardCount => Core.Memory.Read<ushort>(Pointer + Offsets.CardCount);
+
+    public IntPtr ListPtr => Core.Memory.Read<IntPtr>(Pointer + Offsets.ListPtr);
+
+    public TripleTriadCoinExchangeCard[] Cards => Core.Memory.ReadArray<TripleTriadCoinExchangeCard>(ListPtr, CardCount);
+
+    protected AgentTripleTriadCoinExchange(IntPtr pointer) : base(pointer)
+    {
+    }
+}
---
+++ Structs/TripleTriadCoinExchangeCard.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+using Clio.Utilities;
+using ff14bot.Managers;
+
+namespace LlamaLibrary.Structs;
+
+[StructLayout(LayoutKind.Explicit, Size = 0x98)]
+public struct TripleTriadCoinExchangeCard
+{
+    [FieldOffset(0x0)]
+    public uint ItemId;
+
+    [FieldOffset(0x04)]
+    public uint Price;
+
+    [FieldOffset(0x88)]
+    public uint Count;
+
+    [FieldOffset(0x8C)]
+    public uint Count1;
+
+    //This is fucked - ignore
+    /*[FieldOffset(0x40)]
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x20)]
+    public byte[] name_bytes;*/
+
+    public string Name => DataManager.GetItem(ItemId).CurrentLocaleName;
+}
