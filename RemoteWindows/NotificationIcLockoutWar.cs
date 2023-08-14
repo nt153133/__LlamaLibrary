a878c4a783d4bf8bd9c22b44103dde7bcc739597
domesticwarlord86 <domesticwarlord86@gmail.com>
2023-08-14 14:48:30 -0500

    Added ReadyCheck and Lockout windows

---
+++ RemoteWindows/NotificationIcLockoutWar.cs
@@ -0,0 +1,9 @@
+﻿namespace LlamaLibrary.RemoteWindows
+{
+    public class NotificationIcLockoutWar : RemoteWindow<NotificationIcLockoutWar>
+    {
+        public NotificationIcLockoutWar() : base("_NotificationIcLockoutWar")
+        {
+        }
+    }
+}
