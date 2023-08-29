9aac79358616d8138b8909df84fe2d996d981863
nt153133 <52430037+nt153133@users.noreply.github.com>
2023-08-29 14:01:53 -0400

    feat(UIState): HasItem function will check if an item id of a useable item (mount/minion/emote etc) has been unlocked or for regular items if they are in inventories.

--- Helpers/UIState.cs
+++ Helpers/UIState.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Media;
 using ff14bot;
+using ff14bot.Managers;
 using LlamaLibrary.Logging;
 using LlamaLibrary.Memory.Attributes;
+using LlamaLibrary.RetainerItemFinder;
 
 namespace LlamaLibrary.Helpers;
 
@@ -23,6 +27,12 @@ public static class UIState
 
         [Offset("Search 48 8D 0D ? ? ? ? 0F B6 04 08 84 D0 75 ? B8 ? ? ? ? Add 3 TraceRelative")]
         internal static IntPtr MinionArray;
+
+        [Offset("Search E8 ? ? ? ? 48 85 C0 74 ? 66 83 B8 ? ? ? ? ? 0F 84 ? ? ? ? TraceCall")]
+        internal static IntPtr ExdGetItem;
+
+        [Offset("Search E8 ?? ?? ?? ?? 83 F8 01 75 03 TraceCall")]
+        internal static IntPtr IsItemActionUnlocked;
     }
 
     public static IntPtr Instance => Offsets.Instance;
@@ -34,4 +44,66 @@ public static class UIState
     public static byte[] MinionArray => Core.Memory.ReadBytes(Offsets.MinionArray, 0x50);
 
     public static bool MinionUnlocked(int id) => ((1 << (id & 7)) & MinionArray[id >> 3]) > 0;
+
+    public static IntPtr GetItemExdData(uint id) => Core.Memory.CallInjected64<IntPtr>(Offsets.ExdGetItem, id);
+
+    public static bool IsItemActionUnlocked(uint id) => Core.Memory.CallInjected64<int>(Offsets.IsItemActionUnlocked, Offsets.Instance, GetItemExdData(id)) == 1;
+
+    public static bool CanUnlockItem(uint itemId)
+    {
+        var item = DataManager.GetItem(itemId);
+
+        if (item == null)
+        {
+            Log.Error($"Item {itemId} not found in exd");
+            return false;
+        }
+
+        if (item.ItemAction == 0)
+        {
+            return false;
+        }
+
+        return !IsItemActionUnlocked(itemId);
+    }
+
+    /// <summary>
+    /// Checks to see if you have an item unlocked like a mount/hairstyle/emote/minion OR if you have the item in your inventory/retainer/saddlebags/glamour dresser. Ignores HQ/NQ. Force glamour dresser check if you want to make it open the glamour dresser if it's not loaded.
+    /// </summary>
+    /// <param name="itemId">Item Id (use raw id not true id).</param>
+    /// <param name="forceGlamour">Force going to a glamour dresser if no items are cached for it</param>
+    /// <returns>True if you have already acquired or used the item. False otherwise.</returns>
+    public static async Task<bool> HasItem(uint itemId, bool forceGlamour = false)
+    {
+        var item = DataManager.GetItem(itemId);
+
+        if (item == null)
+        {
+            Log.Error($"Item {itemId} not found in data manager");
+            return false;
+        }
+
+        if (item.ItemAction != 0)
+        {
+            return IsItemActionUnlocked(itemId);
+        }
+
+        var inventoryItem = InventoryManager.FilledInventoryAndArmory.Any(i => i.RawItemId == itemId);
+        var retainerItem = WorldHelper.IsOnHomeWorld && (await ItemFinder.SafelyGetCachedRetainerInventories()).Any(i => i.Value.Inventory.Any(j => j.Key % 1000000 == itemId));
+
+        var saddlebagsItem = (await ItemFinder.SafelyGetCachedSaddlebagInventoryComplete()).Inventory.Any(i => i.Key % 1000000 == itemId);
+
+        var glamourDresserItem = false;
+
+        if (!QuestLogManager.IsQuestCompleted(68554))
+        {
+            return inventoryItem || retainerItem || saddlebagsItem || glamourDresserItem;
+        }
+
+        glamourDresserItem = forceGlamour ? (await ItemFinder.GetGlamourDressedUpdated()).Any(i => i % 1000000 == itemId) : ItemFinder.GetCachedGlamourDresserInventory().Any(i => i % 1000000 == itemId);
+
+        //Log.Information($"Item {itemId} found in inventory: {inventoryItem}, retainer: {retainerItem}, saddlebags: {saddlebagsItem}, glamour dresser: {glamourDresserItem}");
+
+        return inventoryItem || retainerItem || saddlebagsItem || glamourDresserItem;
+    }
 }
