0e2d6898afc9859efdc4ffa190a7292799158cda
nt153133 <52430037+nt153133@users.noreply.github.com>
2023-08-29 13:59:23 -0400

    feat: LocationTracker class to remember where you were and return there.
    Create a LocationTracker object and then do your code to move and after call GoBack() on the tracker obj.

---
+++ Helpers/LocationTracking/HouseTravelHelper.cs
@@ -0,0 +1,240 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Buddy.Coroutines;
+using Clio.Utilities;
+using ff14bot;
+using ff14bot.Behavior;
+using ff14bot.Managers;
+using ff14bot.Navigation;
+using ff14bot.Objects;
+using ff14bot.RemoteWindows;
+using LlamaLibrary.Enums;
+using LlamaLibrary.Helpers.Housing;
+using LlamaLibrary.Helpers.HousingTravel;
+using LlamaLibrary.JsonObjects;
+using LlamaLibrary.Logging;
+
+namespace LlamaLibrary.Helpers.LocationTracking;
+
+public static class HouseTravelHelper
+{
+    private static readonly LLogger Log = new LLogger("HouseTravelHelper", Colors.IndianRed);
+    private const uint HouseEntranceId = 2002737;
+    private const uint AptEntranceId = 2007402;
+
+    public static async Task<bool> GoBackToHouse(HouseLocation? previousHouseLocation)
+    {
+        if (previousHouseLocation == null)
+        {
+            return false;
+        }
+
+        var location = CurrentHouseLocation;
+        var skip = false;
+        if (location != null)
+        {
+            if (HousingHelper.IsInsideHouse && location.Equals(previousHouseLocation))
+            {
+                return true;
+            }
+
+            if (HousingHelper.IsWithinPlot)
+            {
+                skip = location.Equals(previousHouseLocation);
+            }
+        }
+
+        if (!skip && !await HousingTraveler.GetToResidential(previousHouseLocation))
+        {
+            return false;
+        }
+
+        if (!HousingHelper.IsWithinPlot)
+        {
+            Log.Error("Not within plot");
+            MovementManager.MoveForwardStart();
+            await Coroutine.Wait(5000, () => HousingHelper.IsWithinPlot);
+            MovementManager.MoveStop();
+        }
+
+        MovementManager.MoveForwardStart();
+        await Coroutine.Sleep(1000);
+        MovementManager.MoveStop();
+
+        //Log.Information("Getting closest housing entrance");
+
+        var entranceIds = new[] { HouseEntranceId, AptEntranceId };
+
+        var locations = GameObjectManager.GetObjectsByNPCIds<GameObject>(entranceIds).OrderBy(x => x.DistanceSqr()).ToList();
+
+        var entrance = locations.FirstOrDefault();
+
+        if (entrance == null)
+        {
+            return false;
+        }
+
+        Log.Information("Found housing entrance, approaching");
+        await Navigation.GroundMove(entrance.Location, 2f);
+
+        if (!entrance.IsWithinInteractRange)
+        {
+            return false;
+        }
+
+        Navigator.NavigationProvider.ClearStuckInfo();
+        Navigator.Stop();
+        await Coroutine.Wait(5000, () => !GeneralFunctions.IsJumping);
+
+        entrance.Interact();
+
+        switch (entrance.NpcId)
+        {
+            // Handle different housing entrance menus
+            case HouseEntranceId:
+            {
+                Log.Information("Entering house");
+
+                if (await Coroutine.Wait(10000, () => SelectYesno.IsOpen))
+                {
+                    SelectYesno.Yes();
+                }
+
+                break;
+            }
+            case AptEntranceId:
+            {
+                Log.Information("Entering apartment");
+
+                if (await Coroutine.Wait(10000, () => SelectString.IsOpen))
+                {
+                    SelectString.ClickSlot(0);
+                }
+
+                break;
+            }
+        }
+
+        if (await Coroutine.Wait(10000, () => CommonBehaviors.IsLoading))
+        {
+            await CommonTasks.HandleLoading();
+        }
+
+        return HousingHelper.IsInsideHouse && CurrentHouseLocation.Equals(previousHouseLocation);
+    }
+
+    internal static async Task<bool> GoBackToPlot(HouseLocation? previousHouseLocation)
+    {
+        var location = CurrentHouseLocation;
+
+        if (previousHouseLocation == null)
+        {
+            Log.Error("Previous house location is null");
+            return false;
+        }
+
+        if (location != null)
+        {
+            if (HousingHelper.IsWithinPlot)
+            {
+                return true;
+            }
+        }
+
+        if (!await HousingTraveler.GetToResidential(previousHouseLocation))
+        {
+            return false;
+        }
+
+        var recorded = HousingTraveler.GetRecordedPlot(previousHouseLocation.HousingZone, previousHouseLocation.Plot);
+
+        await Navigation.GroundMove(recorded.EntranceLocation, 1f);
+
+        if (!HousingHelper.IsWithinPlot)
+        {
+            Log.Error("Not within plot");
+            MovementManager.MoveForwardStart();
+            await Coroutine.Wait(5000, () => HousingHelper.IsWithinPlot);
+            MovementManager.MoveStop();
+        }
+
+        return HousingHelper.IsWithinPlot && CurrentHouseLocation.Equals(previousHouseLocation);
+    }
+
+    internal static async Task<bool> GoBackToHousingLocation(HousingAreaLocation? previousHouseLocation)
+    {
+        if (previousHouseLocation == null)
+        {
+            return false;
+        }
+
+        if (HousingHelper.IsInHousingArea && CurrentHousingAreaLocation.Equals(previousHouseLocation))
+        {
+            return true;
+        }
+
+        if (!await WorldTravel.WorldTravel.GoToWorld(previousHouseLocation.World))
+        {
+            Log.Error("Failed to get to world");
+            return false;
+        }
+
+        if (!await HousingTraveler.GetToResidential(previousHouseLocation.HousingZone, previousHouseLocation.Location, previousHouseLocation.Ward))
+        {
+            Log.Error("Failed to get to housing area");
+            return false;
+        }
+
+        await Navigation.GroundMove(previousHouseLocation.Location, 1f);
+
+        var current = CurrentHousingAreaLocation;
+        return HousingHelper.IsInHousingArea && current != null && current.Ward == previousHouseLocation.Ward && current.HousingZone == previousHouseLocation.HousingZone && current.World == previousHouseLocation.World && current.Location.DistanceSqr(previousHouseLocation.Location) < 10;
+    }
+
+    internal static HouseLocation? CurrentHouseLocation
+    {
+        get
+        {
+            if (!HousingHelper.IsInsideHouse && !HousingHelper.IsWithinPlot)
+            {
+                return null;
+            }
+
+            var info = HousingHelper.HousingPositionInfo;
+            if (!info)
+            {
+                return null;
+            }
+
+            return new HouseLocation(HousingTraveler.TranslateZone((HousingZone)WorldManager.ZoneId), info.Ward, info.Plot);
+        }
+    }
+
+    internal static HousingAreaLocation? CurrentHousingAreaLocation
+    {
+        get
+        {
+            if (!HousingHelper.IsInHousingArea)
+            {
+                return null;
+            }
+
+            return new HousingAreaLocation()
+            {
+                HousingZone = WorldManager.ZoneId,
+                Ward = HousingHelper.HousingPositionInfo.Ward,
+                World = WorldHelper.CurrentWorld,
+                Location = Core.Me.Location
+            };
+        }
+    }
+}
+
+public record HousingAreaLocation
+{
+    public ushort HousingZone { get; set; }
+    public int Ward { get; set; }
+    public World World { get; set; }
+    public Vector3 Location { get; set; }
+}
---
+++ Helpers/LocationTracking/LocationTracker.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Buddy.Coroutines;
+using ff14bot;
+using ff14bot.Behavior;
+using ff14bot.Managers;
+using ff14bot.Navigation;
+using ff14bot.Objects;
+using ff14bot.RemoteWindows;
+using LlamaLibrary.Helpers.Housing;
+using LlamaLibrary.Helpers.NPC;
+using LlamaLibrary.Helpers.WorldTravel;
+using LlamaLibrary.JsonObjects;
+using LlamaLibrary.Logging;
+
+namespace LlamaLibrary.Helpers.LocationTracking;
+
+public class LocationTracker
+{
+    private static readonly LLogger Log = new LLogger("LocationTracker", Colors.IndianRed);
+    private readonly HousingAreaLocation? _previousHousingAreaLocation;
+    private readonly HouseLocation? _previousHouseLocation;
+    private readonly WorldLocation? _previousLocation;
+    private readonly LocationType _previousLocationType;
+    private float _previousHeading;
+
+    public LocationTracker()
+    {
+        Log.Verbose("LocationTracker initialized.");
+        _previousLocation = new WorldLocation(WorldHelper.CurrentWorld, new Location(WorldManager.ZoneId, Core.Me.Location));
+        _previousHeading = Core.Me.Heading;
+        if (!HousingHelper.IsInHousingArea)
+        {
+            _previousLocationType = LocationType.World;
+            return;
+        }
+
+        if (HousingHelper.IsWithinPlot && !HousingHelper.IsInsideHouse)
+        {
+            _previousLocationType = LocationType.HousingPlot;
+            _previousHouseLocation = HouseTravelHelper.CurrentHouseLocation;
+        }
+        else if (HousingHelper.IsInsideHouse)
+        {
+            _previousLocationType = LocationType.House;
+            _previousHouseLocation = HouseTravelHelper.CurrentHouseLocation;
+        }
+        else
+        {
+            _previousLocationType = LocationType.HousingArea;
+            _previousHousingAreaLocation = HouseTravelHelper.CurrentHousingAreaLocation;
+        }
+    }
+
+    public async Task<bool> GoBack()
+    {
+        if (_previousLocation == null)
+        {
+            return false;
+        }
+
+        if (WorldHelper.CurrentWorld != _previousLocation.World)
+        {
+            if (!await WorldTravel.WorldTravel.GoToWorld(_previousLocation.World))
+            {
+                Log.Error("Failed to go back to previous world");
+                return false;
+            }
+        }
+
+        if (_previousLocationType == LocationType.World)
+        {
+            if (GrandCompanyHelper.IsInBarracks && _previousLocation.Location.ZoneId is 130 or 132 or 128)
+            {
+                uint[] npcIds = { 2007528, 2006963, 2007530 };
+                var exitNpc = GameObjectManager.GetObjectsByNPCIds<GameObject>(npcIds).FirstOrDefault();
+                if (exitNpc != null)
+                {
+                    if (await NavigationHelper.InteractWithNpc(exitNpc))
+                    {
+                        if (await Coroutine.Wait(10000, () => SelectYesno.IsOpen))
+                        {
+                            SelectYesno.Yes();
+                            await Coroutine.Wait(10000, () => CommonBehaviors.IsLoading);
+                            Log.Information($"Waiting for loading to finish...");
+                            await Coroutine.Wait(-1, () => !CommonBehaviors.IsLoading);
+                        }
+                    }
+                }
+                else
+                {
+                    Log.Error($"Couldn't find the exit");
+                    //return;
+                }
+            }
+
+            if (!await Navigation.GetTo(_previousLocation) || !await Navigation.GroundMove(_previousLocation.Location.Coordinates, 1f))
+            {
+                Log.Error("Failed to go back to previous location");
+                return false;
+            }
+
+            MovementManager.SetFacing(_previousHeading);
+            return true;
+        }
+
+        switch (_previousLocationType)
+        {
+            case LocationType.HousingArea:
+                Log.Information("Going back to housing area");
+                if (!await HouseTravelHelper.GoBackToHousingLocation(_previousHousingAreaLocation))
+                {
+                    Log.Error($"Failed to get back to housing area {_previousLocation}");
+                    return false;
+                }
+
+                break;
+            case LocationType.House:
+                Log.Information("Going back to house");
+                if (!await HouseTravelHelper.GoBackToHouse(_previousHouseLocation))
+                {
+                    Log.Error($"Failed to get back to house");
+                    return false;
+                }
+
+                break;
+            case LocationType.HousingPlot:
+                Log.Information("Going back to plot");
+                if (!await HouseTravelHelper.GoBackToPlot(_previousHouseLocation))
+                {
+                    Log.Error($"Failed to get back to plot {_previousLocation}");
+                    return false;
+                }
+
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        Log.Information("Going back to previous location");
+        if (!await Navigation.GroundMove(_previousLocation.Location.Coordinates, 1f))
+        {
+            Log.Error("Failed to go back to previous location");
+            return false;
+        }
+
+        MovementManager.SetFacing(_previousHeading);
+        return true;
+    }
+}
+
+public enum LocationType
+{
+    World,
+    HousingArea,
+    House,
+    HousingPlot,
+}
