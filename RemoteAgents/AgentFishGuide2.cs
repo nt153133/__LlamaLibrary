0d7790009b8cc4b7c340f050df16023be74223e4
nt153133 <52430037+nt153133@users.noreply.github.com>
2023-10-04 11:35:16 -0400

    fix(AgentFishGuide2): 6.5 structure changes

--- RemoteAgents/AgentFishGuide2.cs
+++ RemoteAgents/AgentFishGuide2.cs
@@ -9,7 +9,6 @@ using LlamaLibrary.RemoteWindows;
 
 namespace LlamaLibrary.RemoteAgents
 {
-
     public class AgentFishGuide2 : AgentInterface<AgentFishGuide2>, IAgent
     {
         public IntPtr RegisteredVtable => Offsets.Vtable;
@@ -45,6 +44,8 @@ namespace LlamaLibrary.RemoteAgents
 
         public IntPtr EndingPointer => Core.Memory.Read<IntPtr>(InfoPointer + Offsets.EndingPointer);
 
+        public IntPtr FishListPointer(int start = 0) => Core.Memory.Read<IntPtr>(StartingPointer + (8 * start));
+
         public int SlotCount => (int)((EndingPointer.ToInt64() - StartingPointer.ToInt64()) / 8);
 
         public FishGuide2Item[] GetFishListRaw()
@@ -54,7 +55,7 @@ namespace LlamaLibrary.RemoteAgents
 
         public FishGuide2Item[] GetFishListRaw(int start, int count)
         {
-            return Core.Memory.ReadArray<FishGuide2Item>(Core.Memory.Read<IntPtr>(StartingPointer + (8 * start)), count); //.Select(x => x.FishItem) as List<uint>;
+            return Core.Memory.ReadArray<FishGuide2Item>(FishListPointer(start), count); //.Select(x => x.FishItem) as List<uint>;
         }
 
         public async Task<FishGuide2Item[]> GetFishList(int start = 0, int count = 0)
@@ -86,18 +87,29 @@ namespace LlamaLibrary.RemoteAgents
         }
     }
 
-    [StructLayout(LayoutKind.Sequential, Size = 0x10)]
+    //6.5 Size and Layout is different
+#if RB_CN
+    [StructLayout(LayoutKind.Explicit, Size = 0x10)]
     public struct FishGuide2Item
     {
+        //0
+        [FieldOffset(0x0)]
         public uint FishItem;
+        //0x4
+        [FieldOffset(0X4)]
         public ushort Index;
+        //0x6
+        [FieldOffset(0x6)]
         public ushort Unknown;
+        //0x8
+        [FieldOffset(0x8)]
         public ushort Unknown2;
+        //0xA
+        [FieldOffset(0xA)]
         public ushort Unknown5;
+        //0xC
+        [FieldOffset(0xC)]
         public byte bHasCaught;
-        public byte Unknown3;
-        public ushort Unknown4;
-
 
         public bool HasCaught => bHasCaught == 1;
 
@@ -106,5 +118,40 @@ namespace LlamaLibrary.RemoteAgents
             return $"{DataManager.GetItem(FishItem)} : {HasCaught}";
         }
     }
+#else
+    [StructLayout(LayoutKind.Explicit, Size = 0x14)]
+    public struct FishGuide2Item
+    {
+        //0
+        [FieldOffset(0x0)]
+        public uint FishItem;
 
+        //0x4
+        [FieldOffset(0X4)]
+        public ushort Index;
+
+        //0x6
+        [FieldOffset(0x6)]
+        public ushort Unknown;
+
+        //0x8
+        [FieldOffset(0x8)]
+        public ushort Unknown2;
+
+        //0xA
+        [FieldOffset(0xA)]
+        public ushort Unknown5;
+
+        //0xE
+        [FieldOffset(0xE)]
+        public byte bHasCaught;
+
+        public bool HasCaught => bHasCaught == 1;
+
+        public override string ToString()
+        {
+            return $"{DataManager.GetItem(FishItem)} : {HasCaught}";
+        }
+    }
+#endif
 }
