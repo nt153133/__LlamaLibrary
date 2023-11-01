129b49f8f47d56c475d0967ee3aceba9f08a1988
domesticwarlord86 <domesticwarlord86@gmail.com>
2023-11-01 07:01:30 -0500

    feat(FGSEnterDialog): Added base window stuff

---
+++ RemoteWindows/FGSEnterDialog.cs
@@ -0,0 +1,18 @@
+﻿namespace LlamaLibrary.RemoteWindows
+{
+    /// <summary>
+    ///     Class for using the Blunderville registration window.
+    /// </summary>
+    public class FGSEnterDialog : RemoteWindow<FGSEnterDialog>
+    {
+        public FGSEnterDialog() : base("FGSEnterDialog")
+        {
+        }
+
+        public void Register()
+        {
+            SendAction(1, 3, 0);
+        }
+
+    }
+}
