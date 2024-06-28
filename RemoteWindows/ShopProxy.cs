eb137d232cdebff147abb58f31fd9995f632c2e4
zzi <zzi@wolf.gg>
2024-06-28 13:25:34 -0700

    Update ShopProxy.cs

--- RemoteWindows/ShopProxy.cs
+++ RemoteWindows/ShopProxy.cs
@@ -10,6 +10,7 @@ namespace LlamaLibrary.RemoteWindows
         {
             //6.3
             [Offset("Search 8B 5E ? FF 50 ? F6 05 ? ? ? ? ? Add 2 Read8")]
+            [OffsetDawntrail("Search 8B 5B ? FF 50 08 F6 05 ? ? ? ? ? 48 89 44 24 ? C7 44 24 ? ? ? ? ? 48 C7 44 24 ? ? ? ? ? 89 5C 24 68 0F 85 ? ? ? ? 48 8B 0D ? ? ? ? E8 ? ? ? ? 48 8B C8 Add 2 Read8")]
             // pre6.3 [OffsetCN("Search 41 8B 5E ? FF 50 ? F6 05 ? ? ? ? ? Add 3 Read8")]
             internal static int ShopIdPointer;
         }
