5782eba149243bf022e797622c42887c17d08797
zzi <zzi@wolf.gg>
2024-06-29 09:13:45 -0700

    Update EventNpcExtensions.cs

--- Extensions/EventNpcExtensions.cs
+++ Extensions/EventNpcExtensions.cs
@@ -11,6 +11,7 @@ namespace LlamaLibrary.Extensions
         public static class Offsets
         {
             [Offset("Search 44 89 BF ? ? ? ? 83 BF ? ? ? ? ? Add 3 Read32")]
+            [OffsetDawntrail("Search 44 0F 47 F3 44 89 B7 ? ? ? ? Add 7 Read32")]
             internal static int IconID;
         }
 
