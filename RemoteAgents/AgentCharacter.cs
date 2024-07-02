4e980efdf43420ef8be80c2ff7ce756c8cdc7124
nt153133 <52430037+nt153133@users.noreply.github.com>
2024-07-02 15:54:07 -0400

    fix: character agents

--- RemoteAgents/AgentCharacter.cs
+++ RemoteAgents/AgentCharacter.cs
@@ -9,7 +9,7 @@ namespace LlamaLibrary.RemoteAgents
         private static class Offsets
         {
             [Offset("Search 48 8D 05 ? ? ? ? 89 77 ? Add 3 TraceRelative")]
-            [OffsetDawntrail("Search 48 8D 05 ? ? ? ? 48 8B D9 48 89 01 E8 ? ? ? ? 48 8B 4B ? 48 85 C9 74 ? 48 8B 53 ? Add 3 TraceRelative")]
+            [OffsetDawntrail("Search 48 8D 05 ? ? ? ? 89 77 ? 48 8D 4F ? Add 3 TraceRelative")]
             internal static IntPtr Vtable;
         }
 
