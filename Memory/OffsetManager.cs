f964776628ffed72d4bf3243e6022313b145d616
nt153133 <52430037+nt153133@users.noreply.github.com>
2024-07-28 17:38:42 -0400

    fix: Lower Item Quality for DT

--- Memory/OffsetManager.cs
+++ Memory/OffsetManager.cs
@@ -59,7 +59,7 @@ public static class OffsetManager
 
     public static ConcurrentDictionary<string, long> OffsetCache = new();
 
-    private const long _version = 16;
+    private const long _version = 19;
 
     private const bool _debug = false;
 
