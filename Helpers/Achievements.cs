aed8c0a963f0a1d0f0235c8d9cf6a88d15ca82ba
RedWine <redwine@minionway.com>
2024-08-05 10:38:03 -0400

    fix: Fixed HasAchievement in Achievements helper.

--- Helpers/Achievements.cs
+++ Helpers/Achievements.cs
@@ -1,35 +1,29 @@
 ﻿using System;
-using System.Windows.Media;
 using ff14bot;
-using LlamaLibrary.Logging;
 using LlamaLibrary.Memory.Attributes;
 
 namespace LlamaLibrary.Helpers
 {
     public static class Achievements
     {
-        private static readonly LLogger Log = new(nameof(Achievements), Colors.Gold);
-
         private static class Offsets
         {
-            //[Offset("Search 48 8D 0D ? ? ? ? E9 ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? 80 39 ? Add 3 TraceRelative")]
-            //internal static IntPtr Achieve;
+            [Offset("Search E8 ?? ?? ?? ?? 04 30 TraceCall")]
+            [OffsetDawntrail("Search E8 ?? ?? ?? ?? 04 30 FF C3 TraceCall")]
+            internal static IntPtr IsCompletePtr;
 
-            //[OffsetCN("Search 4C 8B C9 81 FA ? ? ? ? 77 ? 8B C2 99 83 E2 ? 03 C2 44 8B C0 83 E0 ? 2B C2 41 C1 F8 ? 49 63 C8 42 0F B6 54 09 ? 8B C8 B8 ? ? ? ? D3 E0 84 D0 0F 95 C0 C3 32 C0 C3 ? ? ? ? ? ? 83 FA ?")]
-            [Offset("Search 4C 8B C9 81 FA ? ? ? ? 77 ? 8B C2 99 83 E2 ? 03 C2 44 8B C0 83 E0 ? 2B C2 41 C1 F8 ? 49 63 C8 42 0F B6 54 09 ? 8B C8 B8 ? ? ? ? D3 E0 84 D0 0F 95 C0 C3 32 C0 C3 ? ? ? ? ? ? 4C 8B C9")]
-            internal static IntPtr CheckById;
+            [Offset("Search 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 04 30 Add 3 TraceRelative")]
+            [OffsetDawntrail("Search 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 04 30 FF C3 Add 3 TraceRelative")]
+            internal static IntPtr AchievementInstancePtr;
         }
 
         public static bool HasAchievement(int achievementId)
         {
             bool done;
+
             lock (Core.Memory.Executor.AssemblyLock)
             {
-                done = Core.Memory.CallInjected64<bool>(
-                    Offsets.CheckById,
-                    UIState.Instance,
-                    achievementId
-                );
+                done = Core.Memory.CallInjected64<bool>(Offsets.IsCompletePtr, Offsets.AchievementInstancePtr, achievementId);
             }
 
             return done;
