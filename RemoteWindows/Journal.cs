3e5ce042348aa9ee5de87893a15ef4496e569edf
nt153133 <52430037+nt153133@users.noreply.github.com>
2024-11-17 13:26:37 -0500

    feat: Add Journal window and select quest

---
+++ RemoteWindows/Journal.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using ff14bot.Managers;
+
+namespace LlamaLibrary.RemoteWindows;
+
+public class Journal : RemoteWindow<Journal>
+{
+    public Journal() : base("Journal")
+    {
+    }
+
+    public void SelectQuest(int globalId)
+    {
+        var rawId = QuestLogManager.ActiveQuests.FirstOrDefault(i => i.GlobalId == globalId)?.RawId ?? 0;
+        if (rawId == 0)
+        {
+            return;
+        }
+
+        SendAction(3, 3, 0xD, 3, rawId, 3, 1);
+    }
+}
