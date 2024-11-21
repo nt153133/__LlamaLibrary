c806b8cf577d424ab23b1c58e6ee5b013139118b
nt153133 <52430037+nt153133@users.noreply.github.com>
2024-11-21 17:32:56 -0500

    feat: Materia affix changes

--- RemoteAgents/AgentMeld.cs
+++ RemoteAgents/AgentMeld.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 using ff14bot;
+using ff14bot.Enums;
 using ff14bot.Managers;
+using LlamaLibrary;
+using LlamaLibrary.JsonObjects;
 using LlamaLibrary.Memory.Attributes;
 
 namespace LlamaLibrary.RemoteAgents
@@ -23,12 +29,22 @@ namespace LlamaLibrary.RemoteAgents
             [Offset("Search 89 83 ? ? ? ? 48 89 83 ? ? ? ? 48 89 83 ? ? ? ? E8 ? ? ? ? 48 8B 8B ? ? ? ? Add 2 Read32")]
             internal static int ItemsToMeldCount;
 
-            [Offset("Search 66 89 83 ? ? ? ? 66 89 83 ? ? ? ? 66 89 83 ? ? ? ? C6 83 ? ? ? ? ? Add 3 Read32")]
+            [Offset("Search 66 89 86 ? ? ? ? E8 ? ? ? ? 85 C0 Add 3 Read32")]
             internal static int IndexOfSelectedItem;
 
             [Offset("Search 0F BF B3 ? ? ? ? 49 8D 8F ? ? ? ? Add 3 Read32")]
             [OffsetDawntrail("Search 0F BF BE ? ? ? ? 4D 8D 64 24 ? Add 3 Read32")]
             internal static int MateriaCount;
+
+            [Offset("Search 48 8B 85 ? ? ? ? 48 0F BF 95 ? ? ? ? Add 3 Read32")]
+            [OffsetCN("Search 48 8B 86 ? ? ? ? 48 0F BF 96 ? ? ? ? Add 3 Read32")]
+            internal static int StructStart;
+
+            [Offset("Search 48 8B 88 ? ? ? ? 4C 8B 04 D1 Add 3 Read32")]
+            internal static int ListPtr;
+
+            [Offset("Search 89 86 ? ? ? ? 48 8B CE E8 ? ? ? ? E9 ? ? ? ? Add 2 Read32")]
+            internal static int SelectedCategory;
         }
 
         protected AgentMeld(IntPtr pointer) : base(pointer)
@@ -44,5 +60,99 @@ namespace LlamaLibrary.RemoteAgents
         public byte IndexOfSelectedItem => Core.Memory.NoCacheRead<byte>(Pointer + Offsets.IndexOfSelectedItem);
 
         public byte MateriaCount => Core.Memory.NoCacheRead<byte>(Pointer + Offsets.MateriaCount);
+
+        public IntPtr StructStart => Core.Memory.Read<IntPtr>(Pointer + Offsets.StructStart);
+
+        public IntPtr ListPtr => Core.Memory.Read<IntPtr>(StructStart + Offsets.ListPtr);
+
+        public IntPtr[] MeldList => Core.Memory.ReadArray<IntPtr>(ListPtr, ItemsToMeldCount).Select(i => Core.Memory.Read<IntPtr>(i)).ToArray();
+
+        public MeldItem[] MeldItems => MeldList.Select(i => Core.Memory.Read<MeldItem>(i)).ToArray();
+
+        public byte SelectedCategory => Core.Memory.NoCacheRead<byte>(Pointer + Offsets.SelectedCategory);
+
+        public int ItemIndex(BagSlot slot)
+        {
+            return MeldItems.ToList().FindIndex(i => i.BagId == slot.BagId && i.BagSlot == slot.Slot);
+        }
+    }
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x48)]
+public struct MeldItem
+{
+    [FieldOffset(0x0)]
+    public IntPtr Pointer;
+
+    [FieldOffset(0x8)]
+    public InventoryBagId BagId;
+
+    [FieldOffset(0xC)]
+    public int BagSlot;
+
+    [FieldOffset(0x10)]
+    public uint ItemId;
+
+    [FieldOffset(0x1C)]
+    private byte _CanOvermeld;
+
+    public bool CanOvermeld => _CanOvermeld == 1;
+
+    [FieldOffset(0x28)]
+    public ushort MateriaType1;
+
+    [FieldOffset(0x2A)]
+    public ushort MateriaType2;
+
+    [FieldOffset(0x2C)]
+    public ushort MateriaType3;
+
+    [FieldOffset(0x2E)]
+    public ushort MateriaType4;
+
+    [FieldOffset(0x30)]
+    public ushort MateriaType5;
+
+    [FieldOffset(0x32)]
+    public byte MateriaGrade1;
+
+    [FieldOffset(0x33)]
+    public byte MateriaGrade2;
+
+    [FieldOffset(0x34)]
+    public byte MateriaGrade3;
+
+    [FieldOffset(0x35)]
+    public byte MateriaGrade4;
+
+    [FieldOffset(0x36)]
+    public byte MateriaGrade5;
+
+    public Item Item => DataManager.GetItem(ItemId);
+
+    public int MateriaCount => new[] { MateriaType1, MateriaType2, MateriaType3, MateriaType4, MateriaType5 }.Count(i => i != 0);
+
+    public MateriaItem[] Materia
+    {
+        get
+        {
+            var materia = new MateriaItem[MateriaCount];
+            for (int i = 0; i < MateriaCount; i++)
+            {
+                var Type = (ushort)GetType().GetField($"MateriaType{i + 1}")?.GetValue(this);
+                var Grade = (byte)GetType().GetField($"MateriaGrade{i + 1}")?.GetValue(this);
+                materia[i] = ResourceManager.MateriaList.Value[Type].First(j => j.Tier == Grade);
+            }
+
+            return materia;
+        }
+    }
+
+    public bool CanMeld => CanOvermeld ? Item.MateriaSlots > MateriaCount : MateriaCount < 5;
+
+    //ToString() method
+    public override string ToString()
+    {
+        return $"{Item?.CurrentLocaleName} {MateriaCount} {CanMeld} {string.Join(',', Materia.Select(i => i.ToString()))}";
     }
 }
--- RemoteWindows/MateriaAttach.cs
+++ RemoteWindows/MateriaAttach.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Buddy.Coroutines;
+using ff14bot.Enums;
+using ff14bot.Managers;
+using LlamaLibrary.Extensions;
 using LlamaLibrary.RemoteAgents;
 
 namespace LlamaLibrary.RemoteWindows;
@@ -12,7 +16,7 @@ public class MateriaAttach : RemoteWindow<MateriaAttach>
 
     public void ClickItem(int index)
     {
-        SendAction(3, 3uL, 1, 3, (ulong)index, 3, 1);
+        SendAction(4, 3uL, 1, 3, (ulong)index, 3, 1, 3, 0);
     }
 
     public void ClickMateria(int index)
@@ -22,7 +26,59 @@ public class MateriaAttach : RemoteWindow<MateriaAttach>
 
     public void ChangeGearDropDown(int index)
     {
+        if (index < 0 || index > 6)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        if (AgentMeld.Instance.SelectedCategory == index)
+        {
+            return;
+        }
+
         SendAction(2, 3, 0, 3, (ulong)index);
+    }
+
+    public void SelectInventory()
+    {
+        ChangeGearDropDown(0);
+    }
+
+    //1 = MainHand/Offhand
+    //2 = Head/Body/Hands
+    //3 = Legs/Feet
+    //4 = Neck/Ears
+    //5 = Wrists/Rings
+    //6 = Equipped Items
+
+    public void SelectMainHandOffHand()
+    {
+        ChangeGearDropDown(1);
+    }
+
+    public void SelectHeadBodyHands()
+    {
+        ChangeGearDropDown(2);
+    }
+
+    public void SelectLegsFeet()
+    {
+        ChangeGearDropDown(3);
+    }
+
+    public void SelectNeckEars()
+    {
+        ChangeGearDropDown(4);
+    }
+
+    public void SelectWristsRings()
+    {
+        ChangeGearDropDown(5);
+    }
+
+    public void SelectEquippedItems()
+    {
+        ChangeGearDropDown(6);
     }
 
     public async Task<bool> OpenMateriaAttachDialog()
@@ -36,6 +92,96 @@ public class MateriaAttach : RemoteWindow<MateriaAttach>
         {
             return true;
         }
+
+        if (!await FindValidMateria())
+        {
+            return false;
+        }
+
+        await Coroutine.Wait(3000, () => AgentMeld.Instance.CanMeld || AgentMeld.Instance.Ready);
+        return Instance.IsOpen;
+    }
+
+    public async Task<bool> OpenMateriaAttachDialog(BagSlot item)
+    {
+        if (!Instance.IsOpen)
+        {
+            item.OpenMeldInterface();
+            if (!await Coroutine.Wait(5000, () => Instance.IsOpen))
+            {
+                return false;
+            }
+        }
+
+        if (MateriaAttachDialog.Instance.IsOpen)
+        {
+            return true;
+        }
+
+        switch (item.BagId)
+        {
+            case InventoryBagId.Bag1:
+            case InventoryBagId.Bag2:
+            case InventoryBagId.Bag3:
+            case InventoryBagId.Bag4:
+            case InventoryBagId.Bag5:
+            case InventoryBagId.Bag6:
+                SelectInventory();
+                await Coroutine.Wait(5000, () => AgentMeld.Instance.SelectedCategory == 0);
+                break;
+            case InventoryBagId.EquippedItems:
+                SelectEquippedItems();
+                await Coroutine.Wait(5000, () => AgentMeld.Instance.SelectedCategory == 6);
+                break;
+            case InventoryBagId.Armory_MainHand:
+            case InventoryBagId.Armory_OffHand:
+                SelectMainHandOffHand();
+                await Coroutine.Wait(5000, () => AgentMeld.Instance.SelectedCategory == 1);
+                break;
+            case InventoryBagId.Armory_Helmet:
+            case InventoryBagId.Armory_Chest:
+            case InventoryBagId.Armory_Glove:
+                SelectHeadBodyHands();
+                await Coroutine.Wait(5000, () => AgentMeld.Instance.SelectedCategory == 2);
+                break;
+            case InventoryBagId.Armory_Pants:
+            case InventoryBagId.Armory_Boots:
+                SelectLegsFeet();
+                await Coroutine.Wait(5000, () => AgentMeld.Instance.SelectedCategory == 3);
+                break;
+            case InventoryBagId.Armory_Earrings:
+            case InventoryBagId.Armory_Necklace:
+                SelectNeckEars();
+                await Coroutine.Wait(5000, () => AgentMeld.Instance.SelectedCategory == 4);
+                break;
+            case InventoryBagId.Armory_Writs:
+            case InventoryBagId.Armory_Rings:
+                SelectWristsRings();
+                await Coroutine.Wait(5000, () => AgentMeld.Instance.SelectedCategory == 5);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        await Coroutine.Sleep(1000);
+
+        var itemIndex = AgentMeld.Instance.ItemIndex(item);
+
+        if (itemIndex == -1)
+        {
+            ff14bot.Helpers.Logging.WriteDiagnostic("Item not found in meld list");
+            return false;
+        }
+
+        Instance.ClickItem(itemIndex);
+
+        if (!await Coroutine.Wait(5000, () => AgentMeld.Instance.IndexOfSelectedItem == itemIndex))
+        {
+            ff14bot.Helpers.Logging.WriteDiagnostic("Item not selected");
+            return false;
+        }
+
+        await Coroutine.Sleep(500);
 
         if (!await FindValidMateria())
         {
