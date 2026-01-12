5d62cb634cf5aa80715a22ed34fda6a0df4af442
Redwine <redwine@minionway.com>
2026-01-12 12:55:45 -0500

    Adding extra functionality to AgentAchievement

--- RemoteAgents/AgentAchievement.cs
+++ RemoteAgents/AgentAchievement.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using ff14bot;
 using ff14bot.Managers;
-using LlamaLibrary.Memory.Attributes;
 using LlamaLibrary.Memory;
 
 namespace LlamaLibrary.RemoteAgents
@@ -9,12 +9,32 @@ namespace LlamaLibrary.RemoteAgents
     public class AgentAchievement : AgentInterface<AgentAchievement>, IAgent
     {
         public IntPtr RegisteredVtable => AgentAchievementOffsets.VTable;
-        
 
         protected AgentAchievement(IntPtr pointer) : base(pointer)
         {
         }
 
         public byte Status => Core.Memory.NoCacheRead<byte>(Pointer + AgentAchievementOffsets.Status);
+
+        private static readonly Dictionary<string, int> VFunctionIds = new()
+        {
+            { "Show", 3 },
+            { "Hide", 5 },
+            { "IsAgentActive", 6 },
+            { "GetAddonId", 9 },
+        };
+
+        private bool IsAgentActive => Core.Memory.CallInjected64<byte>(VFunctionAddress("IsAgentActive"), Pointer) == 1;
+
+        public bool IsOpen => IsAgentActive;
+
+        private IntPtr VFunctionAddress(string function)
+        {
+            var index = VFunctionIds[function];
+
+            return Core.Memory.Read<IntPtr>(Core.Memory.Read<IntPtr>(Pointer) + 0x8 * index);
+        }
+
+
     }
 }
