6d38dacddcca8e8cbafdd8d10259584445b15e43
domesticwarlord86 <domesticwarlord86@gmail.com>
2026-03-03 09:02:16 -0600

    fix: (OffsetManager.cs) Increased game version for Global and China

--- Memory/OffsetManager.cs
+++ Memory/OffsetManager.cs
@@ -107,8 +107,8 @@ public static class OffsetManager
 
         var Records = new Dictionary<ClientRegion, GameRecord>()
         {
-            { ClientRegion.Global, new GameRecord(7.41f, OffsetFlags.Global) },
-            { ClientRegion.China, new GameRecord(7.41f, OffsetFlags.China) },
+            { ClientRegion.Global, new GameRecord(7.45f, OffsetFlags.Global) },
+            { ClientRegion.China, new GameRecord(7.45f, OffsetFlags.China) },
             { ClientRegion.Korea, new GameRecord(7.3f, OffsetFlags.Korea) },
             { ClientRegion.TraditionalChinese, new GameRecord(7.0f, OffsetFlags.TraditionalChinese) },
         };
